a60bc536fb8918d8aebc16420f00a38943b1b96d
Anthony MacAllister <anthonymmacallister@gmail.com>
2018-07-23 20:02:15 -0400

    Added JavaDoc Info

--- src/main/java/net/downwithdestruction/mysticalitems/client/MysticalHatsTab.java
+++ src/main/java/net/downwithdestruction/mysticalitems/client/MysticalHatsTab.java
@@ -5,6 +5,8 @@ import net.downwithdestruction.mysticalitems.item.ModItems;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nonnull;
+
 /**
  * Created by madmac on 7/1/2018.
  */
@@ -15,11 +17,22 @@ public class MysticalHatsTab extends CreativeTabs {
         setBackgroundImageName("item_search.png");
     }
 
+    /**
+     * Displays your item in the custom creative tab
+     *
+     * @return Copper Ingot
+     */
+    @Nonnull
     @Override
     public ItemStack getTabIconItem() {
         return new ItemStack(ModItems.ingotCopper);
     }
 
+    /**
+     * Allows user to search item in search bar
+     *
+     * @return item searched
+     */
     @Override
     public boolean hasSearchBar() {
         return true;
